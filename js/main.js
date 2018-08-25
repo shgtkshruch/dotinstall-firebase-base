79f98d7403f4e9c3422848a818ca3259683cdc06
shgtkshruch <shgtk.shruch@gmail.com>
2018-08-25 21:45:40 +0900

    Render messages with Firebase data

--- js/main.js
+++ js/main.js
@@ -20,6 +20,15 @@ const collection = db.collection('messages');
 
 const message = document.getElementById('message');
 const form = document.querySelector('form');
+const messages = document.getElementById('messages');
+
+collection.get().then(snapshot => {
+  snapshot.forEach(doc => {
+    const li = document.createElement('li');
+    li.textContent = doc.data().message;
+    messages.appendChild(li);
+  });
+});
 
 form.addEventListener('submit', e => {
   e.preventDefault();
