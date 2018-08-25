26594268c7a73c3dc84e6bf1247cd48246723bfb
shgtkshruch <shgtk.shruch@gmail.com>
2018-08-25 21:54:05 +0900

    Completed post form

--- js/main.js
+++ js/main.js
@@ -33,18 +33,22 @@ collection.orderBy('created').get().then(snapshot => {
 form.addEventListener('submit', e => {
   e.preventDefault();
 
+  const val = message.value.trim();
+  if (val === '') return
+
   const li = document.createElement('li');
-  li.textContent = message.value;
+  li.textContent = val;
   messages.appendChild(li);
 
+  message.value = '';
+  message.focus();
+
   collection.add({
-    message: message.value,
+    message: val,
     created: firebase.firestore.FieldValue.serverTimestamp(),
   })
     .then(doc => {
       console.log(`${doc.id} added`);
-      message.value = '';
-      message.focus();
     })
     .catch(error => {
       console.log(error);
