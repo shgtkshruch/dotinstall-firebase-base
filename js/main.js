2a30bbc61908da384e0a0916116baff0b3b2f640
shgtkshruch <shgtk.shruch@gmail.com>
2018-08-26 09:12:19 +0900

    Extract firebase configuration settings

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
+import config from './config.js';
+
 (() => {
   'use strict';
 
-  const config = {
-    apiKey: "AIzaSyB4a2DypoEWh2_gkG9MOhp9Jz4k2HAhczE",
-    authDomain: "myfirebasechatapp-2fe56.firebaseapp.com",
-    databaseURL: "https://myfirebasechatapp-2fe56.firebaseio.com",
-    projectId: "myfirebasechatapp-2fe56",
-    storageBucket: "myfirebasechatapp-2fe56.appspot.com",
-    messagingSenderId: "941607125908"
-  };
   firebase.initializeApp(config);
 
   const db = firebase.firestore();
